527b3caa86de1f1daa3c1c697978d9809e9c5e31
Evgeny <ej.tolk@gmail.com>
2022-10-14 17:52:54 +0300

    feat: some task upd

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -20,9 +20,9 @@ function getMatrixElementsSum(matrix) {
   let sum = 0;
   for (let i = 0; i < matrix.length; i++){
     for (let j = 0; j < matrix[i].length; j++){
-      if (matrix[i][j] > 0){
+      if (i == 0 || matrix[i-1][j] != 0){
         sum = matrix[i][j] + sum;
-      }
+      } 
     }
   }
   return sum;
