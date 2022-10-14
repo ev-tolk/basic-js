8e107e6f692204fb6089ff3c79d281faa2e684e8
Evgeny <ej.tolk@gmail.com>
2022-10-14 16:08:27 +0300

    feat: some task upd

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let cats_number = 0;
+  for (let i = 0; i < matrix.length; i++){
+    for (let j = 0; j < matrix[i].length; j++){
+      if (matrix[i][j] == '^^'){
+        cats_number = cats_number + 1;
+      }
+    }
+  }
+  return cats_number;
 }
 
 module.exports = {
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  let num = String(n);
+  let arr = num.split('');
+  let index = 0;
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] < arr[i+1]){
+      index = i+1;
+    }
+  }
+  let new_arr = arr.splice(index, 1);
+  return Number(new_arr.toString());
 }
 
 module.exports = {
