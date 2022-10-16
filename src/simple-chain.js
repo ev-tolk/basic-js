519f8a52c97d87624a1d5777757e4a0a0e2cb879
Evgeny <ej.tolk@gmail.com>
2022-10-16 16:57:44 +0300

    feat: some task upd

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  currChain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.currChain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.currChain.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if(this._checkIntegerNumber(position)){
+      if(position < 1 || position > this.getLength()){
+        this.currChain.length = 0;
+        throw new Error ("You can't remove incorrect link!");
+      }
+      let index = position -1;
+      this.currChain.splice(index, 1);
+      return this;
+    }
+    this.currChain.length = 0;
+    throw new Error ("You can't remove incorrect link!");
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.currChain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let chain = this.currChain.join('~~');
+    this.currChain.length = 0;
+    return chain;
+  },
+  _checkIntegerNumber(num){
+    return typeof num === 'number' && (num^0) === num;
   }
 };
 
